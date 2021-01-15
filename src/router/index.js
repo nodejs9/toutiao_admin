0ece2445e64711f1f5fe3521382306d420cec030
herenbo <1014408625@qq.com>
2021-01-15 16:57:43 +0800

    修改历史记录

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,15 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import VueRouter from 'vue-router'
 
 Vue.use(Router)
 
-export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
-    }
-  ]
+const routes = [
+
+]
+
+const router =new VueRouter({
+  routes
 })
+
+export default router 
